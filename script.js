407082dfdd28209fe2068bcc0eb3cb79b6e6e19f
Nathan Jones <njonessurvey@gmail.com>
2023-07-18 12:11:07 -0500

    Replace id's with a dot and space out to visualize grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Creates a 16x16 group of divs
 const container = document.getElementById("container");
+container.display = 'flex';
 for(let i = 0; i < 16; i++){
     const col = document.createElement("div");
     col.id = `column${i+1}`;
@@ -8,8 +9,10 @@ for(let i = 0; i < 16; i++){
     for(let r = 0; r < 16; r++){
         const row = document.createElement("div");
         row.id = (`c${i+1}r${r+1}`);
+        row.className = "row";
         console.log(row.id);
-        row.textContent = `${i+1}`;
+        //row.textContent = `C${i+1}R${r+1}`;
+        row.textContent = ".";
         col.appendChild(row); // append the row to the col
     }
     container.appendChild(col); // append the col to the body
