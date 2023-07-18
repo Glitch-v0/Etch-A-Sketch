f9e7fc00f8821b91d6116381a05619e1ee2c2ac3
Nathan Jones <njonessurvey@gmail.com>
2023-07-18 16:33:00 -0500

    Add beach background, beach audio, grid and reset buttons

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// Creates a 16x16 group of divs
+let size = 12;
+function loadout(){
+// Creates a custom-sized group of divs
 const container = document.getElementById("container");
 container.display = 'flex';
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < size; i++){
     const col = document.createElement("div");
     col.id = `column${i+1}`;
+    col.class = 'col';
     console.log(col.id);
     col.style.display = "flex";
-    for(let r = 0; r < 16; r++){
+    for(let r = 0; r < size; r++){
         const row = document.createElement("div");
         row.id = (`c${i+1}r${r+1}`);
         row.className = "row";
@@ -21,7 +24,14 @@ for(let i = 0; i < 16; i++){
 // Get the div element by class name
 var rows = document.getElementsByClassName("row");
 
-// Add the event listener to the div element
+// Set the width and height of each row
+for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    row.style.width = `${window.innerWidth/size*0.45}px`;
+    row.style.height = `${row.style.width}`;
+  }
+
+// Change color when moused over
 for(let div of rows){
     div.addEventListener("mouseover", function(){
         console.log("Mouse is over the row");
@@ -38,4 +48,28 @@ for(let div of rows){
         div.style.backgroundColor = randomColor;
 
 })
+    }}
+
+// Size Button
+const sizeButton = document.getElementById('size');
+sizeButton.addEventListener("click", function(){
+    size = parseInt(prompt("How big would you like your grid? (1-100)"));
+    if(size > 100){
+        size = 100
     }
+    let currentGrid = document.querySelectorAll('.row, .col');
+    currentGrid.forEach(item => {
+        item.remove();
+      });
+    loadout();
+})
+
+// Reset Button
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener("click", function(){
+   let cells = document.getElementsByClassName("row");
+   for(cell of cells){
+    cell.style.backgroundColor = "white";
+   }
+})
+loadout();
