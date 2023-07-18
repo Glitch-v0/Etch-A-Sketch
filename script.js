f7b120195fb1d4f4241921c8d4569ded8bd58ae5
Nathan Jones <njonessurvey@gmail.com>
2023-07-18 12:30:09 -0500

    Change divs to be square objects rather than some text

--- script.js
+++ script.js
@@ -12,8 +12,30 @@ for(let i = 0; i < 16; i++){
         row.className = "row";
         console.log(row.id);
         //row.textContent = `C${i+1}R${r+1}`;
-        row.textContent = ".";
+        //row.textContent = `[...]`;
         col.appendChild(row); // append the row to the col
     }
     container.appendChild(col); // append the col to the body
 }
+
+// Get the div element by class name
+var rows = document.getElementsByClassName("row");
+
+// Add the event listener to the div element
+for(let div of rows){
+    div.addEventListener("mouseover", function(){
+        console.log("Mouse is over the row");
+        // Generate a random number between 0 and 16777215
+        var randomNum = Math.floor(Math.random() * 16777215);
+
+        // Convert the number to a hexadecimal string
+        var randomColor = randomNum.toString(16);
+
+        // Prepend a # sign to the string
+        randomColor = "#" + randomColor;
+
+        // Use the random color as the background color
+        div.style.backgroundColor = randomColor;
+
+})
+    }
